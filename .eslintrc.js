923f48a5551322cd9d372c1cddbc3d07c6564a05
Nguyen Ngoc Dien <diennn@greenglobal.vn>
2021-02-22 13:36:41 +0700

    update eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,10 +13,5 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    semi: ['error', 'always'],
   },
 };
